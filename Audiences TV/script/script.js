7b76b2f7ec48bac00a522872239dedf967124a75
DEBUT Loïc <94994920+DEBUT-Loic@users.noreply.github.com>
2025-07-16 15:22:43 +0200

    Add files via upload

--- Audiences TV/script/script.js
+++ Audiences TV/script/script.js
@@ -32,85 +32,90 @@ async function loadLinks() {
     const data = await res.json();
     
     let rang=1;
-    let total = data.length;
-    data.forEach((elem) => {
-        const idDiv =
-        rang === 1 ? "premier" :
-        rang === 2 ? "deuxieme" :
-        rang === 3 ? "troisieme" :
-        rang < total - 2 ? "milieu" : "fin";
+    let total = data.length-1;
+    console.log(data);
+    data.forEach((elem,index) => {
+        if(index != data.length-1) {
+            const idDiv =
+            rang === 1 ? "premier" :
+            rang === 2 ? "deuxieme" :
+            rang === 3 ? "troisieme" :
+            rang < total - 2 ? "milieu" : "fin";
 
-        if(rang < 4) {
-            // podium
-            $(`#${idDiv} img`).attr("src",`img/${srcImg(elem.chaine)}.png`).attr("alt",elem.chaine);
-            $(`#${idDiv} .programme`).text(elem.programme);
-            $(`#${idDiv} .audience`).text(numStr(elem.audience));
-            $(`#${idDiv} .pda`).text(elem.pda);
-        }
-        else if(rang === 4) {
-            $(`#classement`).prepend(`
-                <div>
-                    <div class="rang">
-                        <p>${rang}</p>
-                    </div>
-                    
-                    <img src="img/${srcImg(elem.chaine)}.png" alt="${elem.chaine}">
-                    <p class="programme_classement">${decodeHTMLEntities(elem.programme)}</p>
-
-                    <div class="audiencePDA">
-                        <div class="audienceDiv">
-                            <p class="audience">${numStr(elem.audience)}</p>
-                            <p>TÉLÉSPECTATEURS</p>
+            if(rang < 4) {
+                // podium
+                $(`#${idDiv} img`).attr("src",`img/${srcImg(elem.chaine)}.png`).attr("alt",elem.chaine);
+                $(`#${idDiv} .programme`).text(decodeHTMLEntities(elem.programme));
+                $(`#${idDiv} .audience`).text(numStr(elem.audience));
+                $(`#${idDiv} .pda`).text(elem.pda);
+            }
+            else if(rang === 4) {
+                $(`#classement`).prepend(`
+                    <div class="card">
+                        <div class="rang">
+                            <p>${rang}</p>
                         </div>
+                        
+                        <img src="img/${srcImg(elem.chaine)}.png" alt="${elem.chaine}">
+                        <p class="programme_classement">${decodeHTMLEntities(elem.programme)}</p>
 
-                        <div class="pdaDiv">
-                            <p class="pda">${elem.pda}</p>
-                            <p>PDA</p>
+                        <div class="audiencePDA">
+                            <div class="audienceDiv">
+                                <p class="audience">${numStr(elem.audience)}</p>
+                                <p>TÉLÉSPECTATEURS</p>
+                            </div>
+
+                            <div class="pdaDiv">
+                                <p class="pda">${elem.pda}</p>
+                                <p>PDA</p>
+                            </div>
                         </div>
                     </div>
-                </div>
-                `)
+                    `)
+            }
+            else {
+                let partie =
+                rang-4 <= Math.floor( (total-4) / 2 ) ? "part1" : "part2";
+
+                $(`#classement #${partie}`).append(`
+                    <div class="${rang == total-3 ? "last-milieu" : idDiv} card">
+                        <div class="rang">
+                            <p>${rang}</p>
+                        </div>
+                        
+                        <img src="img/${srcImg(elem.chaine)}.png" alt="${elem.chaine}">
+                        <p class="programme_classement">${decodeHTMLEntities(elem.programme)}</p>
+
+                        <div class="audiencePDA">
+                            <div class="audienceDiv">
+                                <p class="audience">${numStr(elem.audience)}</p>
+                                <p>TÉLÉSPECTATEURS</p>
+                            </div>
+
+                            <div class="pdaDiv">
+                                <p class="pda">${elem.pda}</p>
+                                <p>PDA</p>
+                            </div>
+                        </div>
+                    </div>
+                    `)
+            };
+
+            rang++;
         }
         else {
-            let partie =
-            rang-4 <= Math.floor( (total-4) / 2 ) ? "part1" : "part2";
-
-            $(`#classement #${partie}`).append(`
-                <div class="${idDiv} ${rang == total-3 ? "last-milieu" : ""}">
-                    <div class="rang">
-                        <p>${rang}</p>
-                    </div>
-                    
-                    <img src="img/${srcImg(elem.chaine)}.png" alt="${elem.chaine}">
-                    <p class="programme_classement">${decodeHTMLEntities(elem.programme)}</p>
-
-                    <div class="audiencePDA">
-                        <div class="audienceDiv">
-                            <p class="audience">${numStr(elem.audience)}</p>
-                            <p>TÉLÉSPECTATEURS</p>
-                        </div>
-
-                        <div class="pdaDiv">
-                            <p class="pda">${elem.pda}</p>
-                            <p>PDA</p>
-                        </div>
-                    </div>
-                </div>
-                `)
+            $(".dateURL").text(data[index]["date"]);
+            let dateMots = data[index]["date"].split(" ");
+            $("body").addClass(dateMots[0].toLowerCase());
+            $("title").text(`${$("title").text()} - ${data[index]["date"]}`)
         }
-
-        // tr.setAttribute("class",classTr);
-
-        // stringHTML += `
-        //     <td class="rang">${rang==1 ? rang+"<sup>er</sup>" : rang+"<sup>e</sup>"}</td>
-        //     <td class="chaine"> <img src="img/${srcImg(elem.chaine)}.png" alt="${elem.chaine}" /> </td>
-        //     <td class="programme">${elem.programme}</td>
-        //     <td class="telespectateur">${numStr(elem.audience)}</td>
-        //     <td class="pda">${elem.pda}</td>
-        //     `;
-
-        rang++;
     });
+    
+    // Exécuter la fonction si la largeur est <= 768px
+    console.log($(window).width() <= 768)
+    if ($(window).width() <= 768) {
+        createImgProgrammeContainer();
+    }
 }
 
 // window.addEventListener('DOMContentLoaded',loadLinks);
@@ -118,7 +123,34 @@ async function loadLinks() {
 window.addEventListener('DOMContentLoaded', async () => {
     try {
         await loadLinks();
+    } catch (error) {
+        console.error("Erreur lors du chargement des liens :", error);
     } finally {
-        document.getElementById('loading').style.display = 'none';
+        $("#loading").animate(
+            { top: "-100%" },
+            1000,
+            () => {
+                $("#loading").hide(); // Optionnel mais recommandé
+            }
+        );
     }
 });
+
+function createImgProgrammeContainer() {
+    const card = $(".card");
+    for(let i=0;i<card.length;i++) {
+        const imgContainer = $('<div class="img-programme-mobile"></div>');
+        
+        imgContainer.append(card.eq(i).find("img"));
+        imgContainer.append(card.eq(i).find(".programme"));
+        imgContainer.append(card.eq(i).find(".programme_classement"));
+
+        if(i < 3) {
+            card.eq(i).find(".info").prepend(imgContainer);
+        }
+        else {
+            card.eq(i).prepend(imgContainer);
+            card.eq(i).prepend(card.eq(i).find(".rang"));
+        }
+    }
+}
